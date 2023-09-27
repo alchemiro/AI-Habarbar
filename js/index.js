2fc33dbceded418a37280ebbeb0c5ff0ec0562d1
fainnoam@gmail.com <fainnoam@gmail.com>
2023-09-27 15:36:26 +0300

    connected project to DB

--- js/index.js
+++ js/index.js
@@ -32,12 +32,12 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
 const NumberOfJudges = 5;
-const projectCol = collection(db, "projects");
+const projectsCollection = collection(db, "projects");
 const guestCol = collection(db, "guests");
 const judgesCol = collection(db, "judges");
 const studentCol = collection(db, "students");
 
-const docRef = doc(projectCol, projectID);
+const docRef = doc(projectsCollection , projectID);
 const project = await getDocs(docRef);
 console.log(project);
 
@@ -72,11 +72,11 @@ class guest extends user {
   async addLike(projectID) {
     if (this.#likes.length < 20) {
       this.#likes.push(projectID);
-      const docRef = doc(projectCol, projectID);
+        const docRef = doc(projectsCollection , projectID);
       const project = await getDocs(docRef);
       console.log(project);
         /*
-      await setDoc(doc(projectCol, projectID), {
+      await setDoc(doc(this.#grades, projectID), {
         likes: updateProjectLikeField + 1,
       });*/
     }
@@ -99,20 +99,51 @@ class Project {
     #img;
     #category;
     #round;
-    constructor(id, name = " ", summary = " ", img = " ", round = 0, category = " ") {
-        /*
-            if(id exist in db):
-                pull the info from the db
-            else:
-                create new doc
-             */
+    
+    constructor(id, name = " ", summary = " ", img = " ", round = 0, category = " ",) {
+
         this.name = name;
         this.#id = id;
         this.#summary = summary;
         this.#img = img;
         this.#round = round;
         this.#category = category;
+        this.#likes = 0;
         this.#grades = new Array(NumberOfJudges);
+        
+        // Check if the project exists
+        const docRef = projectsCollection.doc(id);
+        
+        docRef.get()
+            .then((doc) => {
+                if (doc.exists) {
+                    // Project exists, populate object with Firestore data
+                    const data = doc.data();
+                    this.name = data.name;
+                    this.#summary = data.summary;
+                    this.#img = data.img;
+                    this.#round = data.round;
+                    this.#category = data.category;
+                    this.#likes = data.likes;
+                } else {
+                    // Project doesn't exist, create a new one
+                    return docRef.set({
+                        name: this.name,
+                        summary: this.#summary,
+                        img: this.#img,
+                        round: this.#round,
+                        category: this.#category,
+                        grade: this.#grades,
+                        likes: this.#likes,
+                    });
+                } 
+            })
+            .then(() => {
+                console.log('Project data retrieved or created in Firestore.');
+            })
+            .catch((error) => {
+                console.error('Error checking/creating Firestore project:', error);
+            });
     }
     get id() {
         return this.#id;
@@ -126,5 +157,51 @@ class Project {
     }
     get grades() {
         return this.#grades;
+    }
+    updateInFirestore() {
+        const updatedData = {
+            name: this.name,
+            summary: this.#summary,
+            img: this.#img,
+            round: this.#round,
+            category: this.#category,
+            grade: this.#grades,
+        };
+
+        this.docRef.update(updatedData)
+            .then(() => {
+                console.log('Project data updated in Firestore.');
+            })
+            .catch((error) => {
+                console.error('Error updating data in Firestore:', error);
+            });
+    }
+    AddLike() {
+        this.#likes += 1;
+        const updatedData = {
+            likes: this.#likes,
+        };
+
+        this.docRef.update(updatedData)
+            .then(() => {
+                console.log('likes updated in Firestore.');
+            })
+            .catch((error) => {
+                console.error('Error updating likes in Firestore:', error);
+            });
+    }
+    removeLike() {
+        this.#likes -= 1;
+        const updatedData = {
+            likes: this.#likes,
+        };
+
+        this.docRef.update(updatedData)
+            .then(() => {
+                console.log('likes updated in Firestore.');
+            })
+            .catch((error) => {
+                console.error('Error updating likes in Firestore:', error);
+            });
     }
 }
