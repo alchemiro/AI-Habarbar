a9caff4d979927426301bb46737a91518573e937
fainnoam@gmail.com <fainnoam@gmail.com>
2023-12-30 20:58:23 +0200

    hacksss!

--- js/index.js
+++ js/index.js
@@ -12,11 +12,17 @@ addBtn.addEventListener("click", () => {
 
   getDocument(proj).then((project) => {
     // console.log(gotProj.toString());
-    grid.innerHTML += `<div class="grid-item special">
-    <img src="../img/america.png" alt="pizza" />
-    <p>${project.name}</p>
-    <p>${project.summary}</p>
-    <p>${project.likes}</p>
-    </div>`;
+    grid.innerHTML += `
+    <div class="col-xl-3 col-md-4 col-sm-6">
+                    <div class="card" style="width: 18rem;">
+                        <img src= ${project.Photo} class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">${project.name}</h5>
+                            <p class="card-text">${project.summary}</p>
+                            <div class="card-footer"> ${project.likes} </div>
+                        </div>
+                    </div>
+                </div>
+    `;
   });
 });
