daf252273ba213de5c21d0ef5178a831daf8d95f
fainnoam@gmail.com <fainnoam@gmail.com>
2023-12-30 22:52:17 +0200

    better looking

--- js/index.js
+++ js/index.js
@@ -30,16 +30,15 @@ addBtn.addEventListener("click", async () => {
 
     // console.log(gotProj.toString());
     gridrow.innerHTML += `
-    <div class="col-xl-3 col-md-4 col-sm-6">
-                    <div class="card" style="width: 18rem;">
-                        <img src= ${project.Photo} class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">${project.name}</h5>
-                            <p class="card-text">${project.summary}</p>
-                            <div class="card-footer"> ${project.likes} </div>
-                        </div>
-                    </div>
-                </div>
-    `;
+    <div class="col offset-1">
+        <div class="card" style="width: 18rem;">
+                <img src= ${project.Photo} class="img-thumbnail" style="width: 18rem; height: 18rem;" alt=".../">
+          <div class="card-body">
+              <h5 class="card-title">${project.name}</h5>
+              <p class="card-text">${project.summary}</p>
+              <div class="card-footer"> ${project.likes} </div>
+            </div>
+        </div>
+     </div>`;
   });
 });
