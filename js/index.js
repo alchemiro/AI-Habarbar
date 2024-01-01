1fd5260fdf317ba0d4ae1eedb10be723681e1340
noam5505 <50455647+noam5505@users.noreply.github.com>
2024-01-01 10:31:21 +0200

    syle

--- js/index.js
+++ js/index.js
@@ -17,20 +17,23 @@ addBtn.addEventListener("click", async () => {
     .get();
   const projectsRefMapped = projectsRef.docs.map((doc) => doc.data());
 
-  projectsRefMapped.forEach((project) => {
-    console.log(project.toString());
-
-    // console.log(gotProj.toString());
-    gridrow.innerHTML += `
-    <div class="col offset-1">
-        <div class="card" style="width: 18rem;" onclick="redirectProjectWithParams(${project.id})">
-                <img src= ${project.Photo} class="img-thumbnail" style="width: 18rem; height: 18rem;" alt=".../">
-          <div class="card-body">
-              <h5 class="card-title">${project.name}</h5>
-              <p class="card-text">${project.summary}</p>
-              <div class="card-footer"> ${project.likes} </div>
+ projectsRefMapped.forEach((project) => {
+  console.log(project.toString());
+    project.name = project.name ? "." : project.name; 
+    project.summary = project.summary ? "." : project.summary; 
+  // console.log(gotProj.toString());
+  gridrow.innerHTML += `
+  <div class="col offset-1">
+      <div class="card" style="width: 18rem;">
+              <img src= ${project.Photo} class="img-thumbnail" style="width: 18rem; height: 18rem;" alt=".../">
+        <div class="card-body"> 
+            <h5 class="card-title">${project.name}</h5>
+            <p class="card-text">${project.summary}</p>
+            <div class="card-footer"> ${project.likes} 
+              
             </div>
-        </div>
-     </div>`;
+          </div>
+      </div>
+   </div>`;
   });
 });
