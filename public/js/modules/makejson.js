f3e8179785aa45009287cc6fe99ef545a382d7bd
ghostyboi2793 <ghostyboi2793@gmail.com>
2024-04-03 01:26:41 +0300

    fixed apge

--- public/js/modules/makejson.js
+++ public/js/modules/makejson.js
@@ -1,7 +1,6 @@
-document.getElementById("convertButton").addEventListener("click", () => {
-  const fileInput = document.getElementById("excelFile");
+function sendProjectsToFirebase() {
+  const fileInput = document.getElementById("excelFileProject");
   const file = fileInput.files[0];
-
   if (file) {
     const reader = new FileReader();
 
@@ -16,13 +15,6 @@ document.getElementById("convertButton").addEventListener("click", () => {
 
         const jsonData = XLSX.utils.sheet_to_json(worksheet);
 
-        // You can now use 'jsonData' as needed
-        // document.getElementById("jsonResult").textContent = JSON.stringify(
-        //   jsonData,
-        //   null,
-        //   2
-        // );
-
         jsonData.forEach(async (project) => {
           console.log(project);
           const summary = project.summary ? project.summary : ".";
@@ -31,7 +23,7 @@ document.getElementById("convertButton").addEventListener("click", () => {
           const image = project.img ? project.img : ".";
           const cat = project.category ? project.category : ".";
           const projectObj = new Project(
-            `${project.id}`,
+            project.id,
             name,
             likes,
             summary,
@@ -39,19 +31,92 @@ document.getElementById("convertButton").addEventListener("click", () => {
             cat
           );
           projectCollection
-            .doc(projectObj.id)
+            .doc(projectObj.id.toString())
             .withConverter(projectConverter)
             .set(projectObj);
         });
-
         console.log("done");
-
-        // studentsJson[0]
       } catch (error) {
         console.error("An error occurred:", error);
       }
     };
-
     reader.readAsArrayBuffer(file);
   }
-});
+}
+function sendStudentsToFirebase() {
+  const fileInput = document.getElementById("excelFileStudent");
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      try {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: "array" });
+
+        // Assume the first sheet contains your data
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+        jsonData.forEach(async (student) => {
+          console.log(student);
+
+          const name = student.name;
+          const id = student.id;
+          const project = student.project;
+
+          const studentObj = new Student(id, name, id, 0, project);
+
+          studentCollection
+            .doc(studentObj.id.toString())
+            .withConverter(studentConverter)
+            .set(studentObj);
+        });
+        console.log("done");
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    };
+    reader.readAsArrayBuffer(file);
+  }
+}
+function sendJudgesToFirebase() {
+  const fileInput = document.getElementById("excelFileJudge");
+  const file = fileInput.files[0];
+  if (file) {
+    const reader = new FileReader();
+
+    reader.onload = (e) => {
+      try {
+        const data = new Uint8Array(e.target.result);
+        const workbook = XLSX.read(data, { type: "array" });
+
+        // Assume the first sheet contains your data
+        const sheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[sheetName];
+
+        const jsonData = XLSX.utils.sheet_to_json(worksheet);
+
+        jsonData.forEach(async (judge) => {
+          console.log(judge);
+
+          const id = judge.id;
+          const name = judge.name;
+
+          const judgeObj = new Judge(id, name, id, []);
+
+          judgeCollection
+            .doc(judgeObj.id.toString())
+            .withConverter(judgeConverter)
+            .set(judgeObj);
+        });
+        console.log("done");
+      } catch (error) {
+        console.error("An error occurred:", error);
+      }
+    };
+    reader.readAsArrayBuffer(file);
+  }
+}
